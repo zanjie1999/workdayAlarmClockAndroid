76f2b84d9c04524ad1a541e6157eae771fc4470f
zanjie1999 <zanjie1999@yeah.net>
2023-07-04 11:49:01 +0800

    android普通用户没法chmod+x

--- app/src/main/java/com/zyyme/workdayalarmclock/MainActivity.kt
+++ app/src/main/java/com/zyyme/workdayalarmclock/MainActivity.kt
@@ -1,13 +1,61 @@
 package com.zyyme.workdayalarmclock
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
+    val logViewB = StringBuilder()
+
+    fun print2LogView(s:String?) {
+        if (s != null) {
+            Log.d("logView", s)
+            logViewB.append(s)
+            logViewB.append("\n")
+            findViewById<TextView>(R.id.logView).text = logViewB.toString()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // 复制一下到data分区下
+        try {
+            val inputStream: InputStream = assets.open("workdayAlarmClock-linux-arm")
+            val outputFile = File(filesDir, "workdayAlarmClock")
+            val outputStream: OutputStream = FileOutputStream(outputFile)
+            val buffer = ByteArray(1024)
+            var length: Int
+            while (inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+            outputStream.close()
+            inputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            print2LogView(e.toString())
+        }
 
+        try {
+            val command =
+                "cd " + getFilesDir().getAbsolutePath() + ";pwd;whoami;chmod +x workdayAlarmClock;./workdayAlarmClock"
+            val process = ProcessBuilder("sh", "-c", command)
+                .redirectErrorStream(true)
+                .start()
+
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                print2LogView(line)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            print2LogView(e.toString())
+        }
     }
 }
