0f4be359bef67e482556ff9244156b37b6db0cdd
zanjie1999 <zanjie1999@yeah.net>
2026-01-15 16:40:23 +0800

    修复作为启动器还重复开机启动问题

--- app/src/main/java/com/zyyme/workdayalarmclock/StartReceiver.kt
+++ app/src/main/java/com/zyyme/workdayalarmclock/StartReceiver.kt
@@ -20,6 +20,10 @@ class StartReceiver : BroadcastReceiver() {
             Log.v("workdayAlarmClock", "disabledisabledisable 开机不启动")
             return
         }
+        if (MeService.me != null) {
+            // 已手动启动
+            return
+        }
         Toast.makeText(context, "开机启动咯~", Toast.LENGTH_LONG).show()
         val intent = Intent(context, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
