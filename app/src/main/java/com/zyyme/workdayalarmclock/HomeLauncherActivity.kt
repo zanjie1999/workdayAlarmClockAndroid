737bd9744676e32ce81d548b0f8b14e423590a89
zanjie1999 <zhmie@outlook.com>
2026-08-30 00:11:43 +0800

    可开关的作为桌面开关，不再多编译一份

---
+++ app/src/main/java/com/zyyme/workdayalarmclock/HomeLauncherActivity.kt
@@ -0,0 +1,39 @@
+package com.zyyme.workdayalarmclock
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+
+class HomeLauncherActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        openHomeDestination(runStartupApps = true)
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        openHomeDestination(runStartupApps = false)
+    }
+
+    private fun openHomeDestination(runStartupApps: Boolean) {
+
+        val clockIsVisible = ClockActivity.me?.isActivityStarted == true ||
+                DeskActivity.me?.isActivityStarted == true
+
+        if (runStartupApps && StartupAppHelper.tryHandleLauncherBootActivity(this, intent)) {
+            val startupAppCount = StartupAppHelper.getStartupAppPackageNames(this).size
+            Thread.sleep(startupAppCount * StartupAppHelper.STARTUP_APP_DELAY_MILLIS)
+        }
+
+        val destination = if (clockIsVisible) {
+            Intent(this, AppListActivity::class.java)
+        } else {
+            MeSettings.createClockIntent(this).apply {
+                putExtra("clockMode", true)
+            }
+        }
+        startActivity(destination)
+    }
+}
